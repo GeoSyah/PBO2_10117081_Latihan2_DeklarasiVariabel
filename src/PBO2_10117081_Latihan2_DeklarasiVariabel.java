f6fa20e77b04fd2742e228af621f243e2bc23b9c
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 13:58:17 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan2_DeklarasiVariabel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan cara pendeklarasian variabel
+ */
+public class PBO2_10117081_Latihan2_DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi Nilai ke Variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi Variabel Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+
+        
+        
+        
+    }
+    
+}
